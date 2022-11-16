64511100b5c3793fe470d8f3f6f6f99ca3970abe
Luca Blackwell <luca@lucablackwell.com>
2022-11-16 11:06:33 +0000

    can't use convos so use one function

--- storage/app/holidaychatbot/app/Http/Controllers/BotManController.php
+++ storage/app/holidaychatbot/app/Http/Controllers/BotManController.php
@@ -2,12 +2,16 @@
 namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
 use Illuminate\Http\Request;
 use BotMan\BotMan\Messages\Incoming\Answer;
 use BotMan\BotMan\Cache\LaravelCache;
+use BotMan\BotMan\Cache\SymfonyCache;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class BotManController extends Controller
 {
+    protected $user_name;
 
     /**
      * Place your BotMan logic here.
@@ -16,43 +20,72 @@ class BotManController extends Controller
     {
         $botman = app('botman');
 
-        $botman->hears('{message}', function($bot) {
-            $bot->startConversation(new HolidayConversation);
-        });
+//        $botman = BotManFactory::create([
+//            'config' => [
+//                'user_cache_time' => 30000,
+//                'conversation_cache_time' => 30000,
+//            ],
+//        ], new LaravelCache());
+//
+//        $adapter = new FilesystemAdapter();
+//        $botman = BotManFactory::create([
+//            'config' => [
+//                'user_cache_time' => 30000,
+//                'conversation_cache_time' => 30000,
+//            ],
+//        ], new SymfonyCache($adapter));
 //
 //        $botman->hears('{message}', function($botman, $message) {
-//            $this->askName($botman);
-//
+//            $botman->startConversation(new HolidayConversation);
 //        });
-//
-//        $botman->listen();
 
-    }
+        $botman->hears('{message}', function($botman, $message) {
+            $this->conversation($botman);
 
-//    /**
-//     * Place your BotMan logic here.
-//     */
-//    public function askName($botman)
-//    {
+        });
 //        $botman->ask('Hello! What is your name?', function(Answer $answer) {
 //
 //            $this->user_name = $answer->getText();
+//            $this->reply('It\'s lovely to meet you, ' . $this->user_name . '. Let\'s find you a holiday!');
 //
-//            $this->say('It\'s lovely to meet you, ' . $this->user_name . '. Let\'s find you a holiday!');
+//            //$this->askCountry($this);
 //        });
-//        //$this->askCountry($botman);
-//    }
-//
-//    /**
-//     * Place your BotMan logic here.
-//     */
-//    public function askCountry($botman)
-//    {
-//        $botman->ask('Hello! REATSESTETCOUNTRYname?', function(Answer $answer) {
-//
-//            $this->user_name = $answer->getText();
-//
-//            $this->say('It\'s lovely to meet you, ' . $this->user_name . '. Let\'s find you a holiday!');
-//        });
-//    }
+        $botman->listen();
+
+    }
+
+    /**
+     * Place your BotMan logic here.
+     */
+    public function conversation($botman)
+    {
+        //$botman->startConversation(new HolidayConversation);
+        $botman->ask('Hello! What is your name?', function(Answer $answer) {
+
+            $this->user_name = $answer->getText();
+            $this->say('It\'s lovely to meet you, ' . $this->user_name . '. Let\'s find you a holiday!');
+
+            $this->ask('What country do you live in, ' . $this->user_name . '?', function(Answer $answer) {
+
+                $this->country = $answer->getText();
+
+                $this->say('It\'s lovely to meet you, ' . $this->country . '. Let\'s find you a holiday!');
+            });
+            //$this->askCountry($this);
+        });
+
+    }
+
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askCountry($botman)
+    {
+        $botman->ask('Hello! REATSESTETCOUNTRYname?', function(Answer $answer) {
+
+            $this->user_name = $answer->getText();
+
+            $this->say('It\'s lovely to meet you, ' . $this->user_name . '. Let\'s find you a holiday!');
+        });
+    }
 }
